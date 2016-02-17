80254bc96b214deb963e9681d1462cc3722341c4
iccir919 <iccir919@gmail.com>
2016-02-17 00:04:38 +0000

    Added post to form components sub directory

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+    
+   def new
+       @post = Post.new
+   end
+   
+   def create 
+       @post = Post.new(post_parms)
+       @post.user_id = current_user.id #assign the post to the user that created it
+       respond_to do |f|
+           if(@post.save)
+                f.html {redirect_to "", notice: "Post created!"}
+            else
+                f.html {redirect_to "", notice: "Error: Post Not Saved!"}
+            end 
+        end
+    end
+    
+    private 
+    def post_parms #allows certain data to be passed via form 
+        params.require(:post).permit(:user_id, :content)
+    end
+end
